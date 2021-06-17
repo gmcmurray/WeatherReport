0154478e8066de3014180d6f3383961c830fb884
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-06-16 22:23:38 -0700

    Added ReadMe file, fixed the City select section and local Storage saving and updating of search history.  Used a list to Store History.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # WeatherReport
-API using OpenWeather One Call to show 7 day forecast for multiple cities
+City Weather Report
+
+# Working Files
+HTML,CSS Files: https://github.com/gmcmurray/WeatherReport
+
+Deployed Application: https://gmcmurray.github.io/WeatherReport/
+
+
+## Description 
+
+City Weather report which provides current and 5-day forecast weather
+by making calls from Open Weather API.  User inputs city and 
+API provides temperature, wind, humidity and UVI index.
+Weather Icons are provided to dgive a visual queue for
+weather conditions.  UVI index is provided and health alerts
+(low, moderate and high) are provided through a color coded
+button.
+
+
+## Skills and Coding Features
+Third party API - Open Weather was used to retrieve given city weather data.
+Three separate fetch() calls were required to achieve API requirements.
+1) weather => todays weather conditions retrieved using city
+2) onecall => UVI index, required lat and lon of city to  retrieve data
+3) forecast => 5 day forecast 
+Fetch command along with JSON.parse() was used to retireve targeted data.
+Materialize CSS was used as framework to style page.
+
+## Screen Shots
+Screen shots with width of 550 pixels below.
+
+![550 pixel width screen shot of Portfolio.](./assets/Images/WeatherReport550px.png)
+
+Page 1 - Main Page
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
 var currrentDay = $('#currentDay');
-var cityforecast=$('#forecast');
-var cityweather=$('#cityweather');
-var cityweathertoday=$('#cityweathertoday');
-
-// var timeblocks = $('#timeblocks');
+var cityforecast = $('#forecast');
+var cityweather = $('#cityweather');
+var cityweathertoday = $('#cityweathertoday');
+var uviseverity = "";
 var now = moment();
 var clor;
-var city
-//  ='Olympia';
-var colors=["green","yellow","red","white","brown"];
-
+var city;
+var colors = ["red", "yellow", "green", "blue", "purple"];
 
 // Gets today's weather from city
 var getweather = function (city) {
@@ -19,87 +16,131 @@ var getweather = function (city) {
       if (response.ok) {
         response.json().then(function (data) {
           populateweather(data, city);
-          return(true);
+          return (true);
         });
       } else {
         alert('Error: ' + response.statusText);
-        return(false);
+        return (false);
       }
     })
     .catch(function (error) {
       alert('Unable to connect to OpenWeather');
     });
 };
-var setuvindexicon = function(data){
-  console.log("seticonuvicalled")
-if(data.current.uvi <= 2){
-  $('#uvibtn').addClass("green");
-  return("low");
-}
-else if(data.current.uvi <=7){
-$('#uvibtn').addClass("yellow");
-return("moderate");
-}
-else {
-  $('#uvibtn').addClass("red");
-  return("high")
-}
+
+// Populates Daily Weather information
+var populateweather = function (data, city) {
+  var iconpath = "./assets/Images/" + chooseicon(data);
+  document.getElementById("cityw").src = iconpath;
+  $('#tempnow').text((data.main.temp - 273.15).toFixed(1));
+  $('#windnow').text((data.wind.speed).toFixed(1));
+  $('#humiditynow').text((data.main.humidity).toFixed(1));
+  $('#selectedcity').text(city);
+  $('#today').text(now.format("MM/D/YY"))
+  localStorage.setItem("lon", data.coord.lon)
+  localStorage.setItem("lat", data.coord.lat)
+  return;
 }
 
-$('#uvibtn').click(function(e){
-if(uviseverity==="low"){
-  alert("No Protection Required. You can safely stay outside")
+// Chooses current weather icon based on description
+var chooseicon = function (data) {
+  if (data.weather[0].description === "clear sky") {
+    return ("sunny.png")
+  }
+  else if (data.weather[0].description === "rain") {
+    return ("rain.png")
+  }
+  else if (data.weather[0].description === "shower rain") {
+    return ("rainchance.png")
+  }
+  else if (data.weather[0].description === "snow") {
+    return ("snow.png")
+  }
+  else if (data.weather[0].description === "mist") {
+    return ("FogHazy.png")
+  }
+  else if (data.weather[0].description === "thunderstorm") {
+    return ("ThunderStrom.png")
+  }
+  else if (data.weather[0].description === "scattered clouds" || data.weather[0].description === "broken clouds" || data.weather[0].description === "overcast clouds") {
+    return ("cloudy.png")
+  }
+  else if (data.weather[0].description === "few clouds") {
+    return ("cloudysunny.png")
+  }
+  else { return ("unknown.png") }
 }
-if(uviseverity==="moderate"){
-  alert("Protection Required. Seek shade during midday hours. Wear shirt, sunscreen and use hat.")
-}
-if(uviseverity==="high"){
-  alert("Extra Protection Required.  Avoid being outside during midday hours. Shirt, sunscreen and hat are a must!")
-}
-});
 
-
-var uviseverity = "";
+// UVI data requires separate API call which is done after 
+// daily weather call to retrieve lat and lon (stored localStorage)
+// Sets UVI button color and classification based on index value
 
 // Gets UVI reading of city - needs lat, lon to work which is stored in localStorage after getweather/populateweather
-var getUVI = function(){
+var getUVI = function () {
   var citylat = localStorage.getItem("lat");
   var citylon = localStorage.getItem("lon");
   var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${citylat}&lon=${citylon}&appid=6c739212e839903aab1ecb07a7173d6b`;
   fetch(apiUrl)
-  .then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log("UVIdat", data)
-        // 
-        $('#UVInow').text((data.current.uvi).toFixed(1));
-        uviseverity=setuvindexicon(data);
-        return(true);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-      return(false);
-    }
-  })
-  .catch(function (error) {
-    alert('Unable to connect to OpenWeather');
-  });
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          $('#UVInow').text((data.current.uvi).toFixed(1));
+          uviseverity = setuvindexicon(data);
+          return (true);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+        return (false);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to OpenWeather');
+    });
 };
 
-// Gets 5 day forecast
+// Sets uvi button color and severity level based on index value
+var setuvindexicon = function (data) {
+  if (data.current.uvi <= 2) {
+    $('#uvibtn').addClass("green");
+    return ("low");
+  }
+  else if (data.current.uvi <= 7) {
+    $('#uvibtn').addClass("yellow");
+    return ("moderate");
+  }
+  else {
+    $('#uvibtn').addClass("red");
+    return ("high")
+  }
+}
+
+// Provides alerts and advice on UVI index value
+$('#uvibtn').click(function (e) {
+  if (uviseverity === "low") {
+    alert("No Protection Required. You can safely stay outside")
+  }
+  if (uviseverity === "moderate") {
+    alert("Protection Required. Seek shade during midday hours. Wear shirt, sunscreen and use hat.")
+  }
+  if (uviseverity === "high") {
+    alert("Extra Protection Required.  Avoid being outside during midday hours. Shirt, sunscreen and hat are a must!")
+  }
+});
+
+
+// Gets 5 day forecast of city
 var getforecastweather = function (city) {
   var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&exclude&appid=6c739212e839903aab1ecb07a7173d6b`;
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          console.log("firstitem",data.list[0]);
           populateforecast(data, city);
-          return(true);
+          return (true);
         });
       } else {
         alert('Error: ' + response.statusText);
-        return(false);
+        return (false);
       }
     })
     .catch(function (error) {
@@ -107,108 +148,28 @@ var getforecastweather = function (city) {
     });
 };
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.autocomplete');
-//   var instances = M.Autocomplete.init(elems, options);
-// });
 
-$("#currentDay").text(now.format("dddd, MMMM D, YYYY"));
-
-var chooseicon = function(data){
-  if(data.weather[0].description==="clear sky"){
-    return("sunny.png")
-  }
-  else if(data.weather[0].description==="rain"){
-    return("rain.png")
-  }
-  else if(data.weather[0].description==="shower rain"){
-    return("rainchance.png")
-  }
-  else if(data.weather[0].description==="snow"){
-    return("snow.png")
-  }
-  else if(data.weather[0].description==="mist"){
-    return("FogHazy.png")
-  }
-  else if(data.weather[0].description==="thunderstorm"){
-    return("ThunderStrom.png")
-  }
-  else if(data.weather[0].description==="scattered clouds" || data.weather[0].description==="broken clouds"||data.weather[0].description==="overcast clouds"){
-    return("cloudy.png")
-  }
-  else if(data.weather[0].description==="few clouds"){
-    return("cloudysunny.png")
-  }
-  else{return("unknown.png")}
-}
-
-var  populateweather = function(data,city){
-  var iconpath = "./assets/Images/"+ chooseicon(data);
-  console.log("iconpath",iconpath)
-  console.log("today", data)
-  document.getElementById("cityw").src=iconpath;
-  $('#tempnow').text((data.main.temp - 273.15).toFixed(1));
-  $('#windnow').text((data.wind.speed).toFixed(1));
-  $('#humiditynow').text((data.main.humidity).toFixed(1));
-  $('#selectedcity').text(city);
-  $('#today').text(now.format("MM/D/YY"))
-  localStorage.setItem("lon",data.coord.lon)
-  localStorage.setItem("lat",data.coord.lat)
-  return;
-}
-
-
-
-var chooseiconfore = function(data,index){
-  if(data.list[index*8].weather[0].description==="clear sky"){
-    return("sunny.png")
-  }
-  else if(data.list[index*8].weather[0].description==="rain"){
-    return("rain.png")
-  }
-  else if(data.list[index*8].weather[0].description==="shower rain"){
-    return("rainchance.png")
-  }
-  else if(data.list[index*8].weather[0].description==="snow"){
-    return("snow.png")
-  }
-  else if(data.list[index*8].weather[0].description==="mist"){
-    return("FogHazy.png")
-  }
-  else if(data.list[index*8].weather[0].description==="thunderstorm"){
-    return("ThunderStrom.png")
-  }
-  else if(data.list[index*8].weather[0].description==="scattered clouds" ||data.list[index*8].weather[0].description==="overcast clouds" || data.list[index*8].weather[0].description==="broken clouds"){
-    return("cloudy.png")
-  }
-  else if(data.list[index*8].weather[0].description==="few clouds"){
-    return("cloudysunny.png")
-  }
-  else{return("unknown.png")}
-}
-
-var populateforecast = function(data,city){
-  console.log("forecast",data.list[1].main.humidity)
-    for (let index = 1; index < 6; index++) {
-      var avgwind=0;
-      var avgtemp=0;
-      var avghumidity=0;
-      var weatherdescript =data.list[(index-1)*8].weather[0].description
-      var weathericon = "./assets/Images/"+ chooseiconfore(data,index-1);
-      for (var y=0; y<8; y++){
-        avgwind+=data.list[(index-1)*8+y].wind.speed/8;
-        avgtemp+=data.list[(index-1)*8+y].main.temp/8;
-        avghumidity+=data.list[(index-1)*8+y].main.humidity/8;
-      }
-      console.log("avghumidity",avghumidity)
-      avgwind=avgwind.toFixed(1);
-      avghumidity=avghumidity.toFixed(1);
-      avgtemp-=273.15;
-      avgtemp=avgtemp.toFixed(1);
-      var date=moment().add(index,"days")
-      console.log(weathericon)
+// Populates Forecast Cards, uses 5 averages of 8 points for daily number
+var populateforecast = function (data, city) {
+  cityforecast.empty();
+  for (let index = 1; index < 6; index++) {
+    var avgwind = 0;
+    var avgtemp = 0;
+    var avghumidity = 0;
+    var weatherdescript = data.list[(index - 1) * 8].weather[0].description
+    var weathericon = "./assets/Images/" + chooseiconfore(data, index - 1);
+    for (var y = 0; y < 8; y++) {
+      avgwind += data.list[(index - 1) * 8 + y].wind.speed / 8;
+      avgtemp += data.list[(index - 1) * 8 + y].main.temp / 8;
+      avghumidity += data.list[(index - 1) * 8 + y].main.humidity / 8;
+    }
+    avgwind = avgwind.toFixed(1);
+    avghumidity = avghumidity.toFixed(1);
+    avgtemp -= 273.15;
+    avgtemp = avgtemp.toFixed(1);
+    var date = moment().add(index, "days")
     cityforecast.append(`
-    <div class="card col ${colors[index-1]} lighten-3">
+    <div class="card col ${colors[index - 1]} lighten-3">
     <div class="row">
     <h5 id="cadtit" style="font-size:15px;font-weight: 900" class="card-title activator small black-text">${date.format(" ddd : MM/D/YY")}</h5>
      <ul >
@@ -224,29 +185,78 @@ var populateforecast = function(data,city){
   </div> 
       `)
 
-    }     
+  }
 }
 
 
-document.getElementById("lastcity").addEventListener('click',goforit);
-document.getElementById("cityselectbt").addEventListener('click',goforitreg);
-function goforit(){
-
-  var city=$('#lastcity').text();
-  // var city="hello"
-  console.log("city1" , city)
-  getweather(city);   
-  getUVI()
-  getforecastweather(city);
+// Chooses icon for Forecast section using list[40] selecting every 8th value
+var chooseiconfore = function (data, index) {
+  if (data.list[index * 8].weather[0].description === "clear sky") {
+    return ("sunny.png")
+  }
+  else if (data.list[index * 8].weather[0].description === "rain") {
+    return ("rain.png")
+  }
+  else if (data.list[index * 8].weather[0].description === "shower rain") {
+    return ("rainchance.png")
+  }
+  else if (data.list[index * 8].weather[0].description === "snow") {
+    return ("snow.png")
+  }
+  else if (data.list[index * 8].weather[0].description === "mist") {
+    return ("FogHazy.png")
+  }
+  else if (data.list[index * 8].weather[0].description === "thunderstorm") {
+    return ("ThunderStrom.png")
+  }
+  else if (data.list[index * 8].weather[0].description === "scattered clouds" || data.list[index * 8].weather[0].description === "overcast clouds" || data.list[index * 8].weather[0].description === "broken clouds") {
+    return ("cloudy.png")
+  }
+  else if (data.list[index * 8].weather[0].description === "few clouds") {
+    return ("cloudysunny.png")
+  }
+  else { return ("unknown.png") }
 }
 
-function goforitreg(){
-  var city=$('#cityselected').val();
-  // var city="hello"
-  console.log("city1" , city)
-  getweather(city);   
-  getUVI()
-  getforecastweather(city);
+// Searchhistory initial definition for 5  history calls
+var search_history = ["last", "second", "third", "fourth", "fifth"];
+if(localStorage.getItem("search_history")===null){
+  localStorage["search_history"] = JSON.stringify(search_history);
+}
+
+var updateSearchhistory = function (city) {
+  var stored_history = JSON.parse(localStorage["search_history"]);
+  stored_history.unshift(city);
+  stored_history.pop();
+  localStorage["search_history"] = JSON.stringify(stored_history);
+  return (true);
+}
+
+// Pulls from localStorage Search history
+var reloadSearchhistory = function(){
+  stored_history = JSON.parse(localStorage["search_history"]);
+  console.log("4", stored_history)
+  for (let index = 0; index < stored_history.length; index++) {
+    var x = index.toString() + "city"
+    $("#" + x).text(stored_history[index]);
+    console.log("reload searchhistory", $("#" + x).val())
+  }
+  return (true);
 }
 
 
+// Execution Code - Main
+
+$("#currentDay").text(now.format("dddd, MMMM D, YYYY"));
+
+reloadSearchhistory();
+
+$("#cityselectbt").click(
+  function (event) {
+    reloadSearchhistory();
+    city = $("#cityselected").val();
+    getweather(city);
+    getUVI()
+    getforecastweather(city);
+    updateSearchhistory(city);
+  });
